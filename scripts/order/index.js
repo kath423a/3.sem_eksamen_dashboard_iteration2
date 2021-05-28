349fc93da6267eaa0decd25f4af6614e036ac2c6
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-05-29 00:15:33 +0200

    Added icons , default message to lists to order page and  favicon to all pages

--- scripts/order/index.js
+++ scripts/order/index.js
@@ -10,176 +10,198 @@ let allOrders = [];
 let filter = "queue";
 
 const Order = {
-  orderid: 0,
-  time: 0,
-  total: 0,
-  quantity: 0,
-  image: "",
-  items: [],
+    orderid: 0,
+    time: 0,
+    total: 0,
+    quantity: 0,
+    image: "",
+    items: [],
 };
 
 //Get the order data
 function start() {
-  console.log("lets gooooo");
+    console.log("lets gooooo");
 
-  loadJSON();
+    loadJSON();
 
-  // Add event-listeners to filter buttons
-  registerButtons();
+    // Add event-listeners to filter buttons
+    registerButtons();
 }
 
 function registerButtons() {
-  document.querySelectorAll("[data-order]").forEach((button) => button.addEventListener("click", selectFilter));
-  document.querySelector(".accept_order").addEventListener("click", () => {
-    const alertMessage = document.querySelector(".alert_message");
-    alertMessage.classList.remove("hidden");
-    setTimeout(function () {
-      alertMessage.classList.add("hidden");
-    }, 5000);
-  });
+    document
+        .querySelectorAll("[data-order]")
+        .forEach((button) => button.addEventListener("click", selectFilter));
+    document.querySelector(".accept_order").addEventListener("click", () => {
+        const alertMessage = document.querySelector(".alert_message");
+        alertMessage.classList.remove("hidden");
+        setTimeout(function () {
+            alertMessage.classList.add("hidden");
+        }, 5000);
+    });
 }
 
 //Get the array
 function loadJSON() {
-  fetch("https://hold-kaeft-vi-har-det-godt.herokuapp.com/")
-    .then((response) => response.json())
-    .then((jsonData) => {
-      // when loaded, prepare objects
-      data = jsonData;
-      console.log(jsonData);
-      prepareObjects(jsonData[filter]);
+    fetch("https://hold-kaeft-vi-har-det-godt.herokuapp.com/")
+        .then((response) => response.json())
+        .then((jsonData) => {
+            // when loaded, prepare objects
+            data = jsonData;
+            console.log(jsonData);
+            prepareObjects(jsonData[filter]);
 
-      setTimeout(loadJSON, 5000);
-    });
+            setTimeout(loadJSON, 5000);
+        });
 }
 
 function prepareObjects(jsonData) {
-  const orders = [];
+    const orders = [];
 
-  jsonData.forEach((jsonObject) => {
-    const order = Object.create(Order);
+    jsonData.forEach((jsonObject) => {
+        const order = Object.create(Order);
 
-    //Get the correct timesyntax
-    const correctTime = dayjs(jsonObject.startTime).format("hh:mm:ss");
+        //Get the correct timesyntax
+        const correctTime = dayjs(jsonObject.startTime).format("hh:mm:ss");
 
-    order.orderid = jsonObject.id;
-    order.time = correctTime;
-    order.items = jsonObject.order;
-    //order.total =
+        order.orderid = jsonObject.id;
+        order.time = correctTime;
+        order.items = jsonObject.order;
+        //order.total =
 
-    orders.push(order);
-    allOrders.push(order);
-  });
-  displayList(orders);
+        orders.push(order);
+        allOrders.push(order);
+    });
+    displayList(orders);
 }
 
 function displayList(orders) {
-  //clear the list
-  document.querySelector(".js_orders_list").innerHTML = "";
+    //clear the list
+    const orderList = document.querySelector(".js_orders_list");
+    orderList.innerHTML = "";
 
-  //build a new list
-  orders.forEach(displayOrder);
+    // If there is at least one order - Show the order
+    if (orders.length >= 1) {
+        //build a new list
+        orders.forEach(displayOrder);
+        console.log("There is orders");
+    } else {
+        // Else show a message
+        orderList.innerHTML = `<p class="message">No ${filter} orders</p>`;
+    }
 }
 
 function displayOrder(order) {
-  //create clone
-  const clone = document.querySelector("#order_item").content.cloneNode(true);
+    //create clone
+    const clone = document.querySelector("#order_item").content.cloneNode(true);
 
-  //set clone data
-  clone.querySelector(".order_id").textContent = ` #${order.orderid}`;
-  clone.querySelector(".time").textContent = order.time;
-  clone.querySelector(".total").textContent = order.items.length * 40;
-  console.log(order);
+    //set clone data
+    clone.querySelector(".order_id").textContent = ` #${order.orderid}`;
+    clone.querySelector(".time").textContent = order.time;
+    clone.querySelector(".total").textContent = order.items.length * 40;
+    console.log(order);
 
-  clone.querySelector(".orders_pop").addEventListener("click", () => showSingleOrder(order));
+    clone
+        .querySelector(".orders_pop")
+        .addEventListener("click", () => showSingleOrder(order));
 
-  document.querySelector(".js_orders_list").appendChild(clone);
+    document.querySelector(".js_orders_list").appendChild(clone);
 }
 
 function selectFilter() {
-  console.log(this.dataset.order);
+    console.log(this.dataset.order);
 
-  filter = this.dataset.order;
-  console.log("filter is: ", filter);
+    filter = this.dataset.order;
+    console.log("filter is: ", filter);
 
-  document.querySelectorAll(".order_status_tabs button").forEach((button) => {
-    button.classList.remove("is_active");
-  });
+    document.querySelectorAll(".order_status_tabs button").forEach((button) => {
+        button.classList.remove("is_active");
+    });
 
-  this.classList.add("is_active");
+    this.classList.add("is_active");
 
-  document.querySelector(".js_orders_list").innerHTML = "";
-  prepareObjects(data[this.dataset.order]);
+    document.querySelector(".js_orders_list").innerHTML = "";
+    prepareObjects(data[this.dataset.order]);
 }
 
 function showSingleOrder(order) {
-  const randomCustomerName = getRandomCustomerName();
-  console.log("Showing data to the order view");
+    const randomCustomerName = getRandomCustomerName();
+    console.log("Showing data to the order view");
 
-  document.querySelector(".order_status_info .order_id").textContent = ` #${order.orderid}`;
-  document.querySelector(".order_status_info .time").textContent = order.time;
-  document.querySelector(".order_status_info .customer_name").textContent = randomCustomerName;
+    document.querySelector("#order_info .message").classList.add("hidden");
+    document
+        .querySelector("#order_info .inner_wrapper")
+        .classList.remove("hidden");
 
-  displayBeers(order.items);
+    document.querySelector(
+        ".order_status_info .order_id"
+    ).textContent = ` #${order.orderid}`;
+    document.querySelector(".order_status_info .time").textContent = order.time;
+    document.querySelector(".order_status_info .customer_name").textContent =
+        randomCustomerName;
+
+    displayBeers(order.items);
 }
 
 function displayBeers(beers) {
-  //clear the list
-  document.querySelector(".js_beer_list").innerHTML = "";
+    //clear the list
+    document.querySelector(".js_beer_list").innerHTML = "";
 
-  const sortedBeers = [];
-  let price = 0;
+    const sortedBeers = [];
+    let price = 0;
 
-  beers.forEach((beer) => {
-    const beerObject = {
-      name: beer,
-      quantity: 1,
-      price: 40,
-    };
-    price = price + 40;
-    //check om øllen er der i forvejen
-    if (sortedBeers.some((e) => e.name === beer)) {
-      const findBeer = sortedBeers.find((e) => e.name === beer);
-      findBeer.quantity = findBeer.quantity + 1;
-      console.log("der er allerede en øl med samme navn");
-    } else {
-      sortedBeers.push(beerObject);
-      console.log("der er IKKE en øl med samme navn");
-    }
-  });
-  console.log("sortedBeers", sortedBeers);
-  console.log("beers", beers);
+    beers.forEach((beer) => {
+        const beerObject = {
+            name: beer,
+            quantity: 1,
+            price: 40,
+        };
+        price = price + 40;
+        //check om øllen er der i forvejen
+        if (sortedBeers.some((e) => e.name === beer)) {
+            const findBeer = sortedBeers.find((e) => e.name === beer);
+            findBeer.quantity = findBeer.quantity + 1;
+            console.log("der er allerede en øl med samme navn");
+        } else {
+            sortedBeers.push(beerObject);
+            console.log("der er IKKE en øl med samme navn");
+        }
+    });
+    console.log("sortedBeers", sortedBeers);
+    console.log("beers", beers);
 
-  //build a new list
-  sortedBeers.forEach(showBeer);
-  showPrice(price);
+    //build a new list
+    sortedBeers.forEach(showBeer);
+    showPrice(price);
 }
 
 function showPrice(price) {
-  document.querySelector(".bottom_info .total_total").textContent = price;
+    document.querySelector(".bottom_info .total_total").textContent = price;
 }
 
 function showBeer(beer) {
-  //create clone
-  const clone = document.querySelector("#beer_orders").content.cloneNode(true);
+    //create clone
+    const clone = document
+        .querySelector("#beer_orders")
+        .content.cloneNode(true);
 
-  //set clone data
-  clone.querySelector(".beer_image").src = `/images/${beer.name}.png`;
-  clone.querySelector(".beer_name").textContent = beer.name;
-  clone.querySelector(".beer_price").textContent = `${beer.price},-`;
-  clone.querySelector(".quantity").textContent = `${beer.quantity}x`;
-  clone.querySelector(".price").textContent = beer.price * beer.quantity;
+    //set clone data
+    clone.querySelector(".beer_image").src = `/images/${beer.name}.png`;
+    clone.querySelector(".beer_name").textContent = beer.name;
+    clone.querySelector(".beer_price").textContent = `${beer.price},-`;
+    clone.querySelector(".quantity").textContent = `${beer.quantity}x`;
+    clone.querySelector(".price").textContent = beer.price * beer.quantity;
 
-  document.querySelector(".js_beer_list").appendChild(clone);
+    document.querySelector(".js_beer_list").appendChild(clone);
 }
 
 function getRandomCustomerName() {
-  const customers = settings.randomCustomers;
-  const customerAmount = customers.length;
-  console.log(customerAmount);
+    const customers = settings.randomCustomers;
+    const customerAmount = customers.length;
+    console.log(customerAmount);
 
-  const randomNumber = getRandomInteger(1, customerAmount);
-  const randomCustomer = customers[randomNumber - 1];
+    const randomNumber = getRandomInteger(1, customerAmount);
+    const randomCustomer = customers[randomNumber - 1];
 
-  return randomCustomer;
+    return randomCustomer;
 }
